20b573430aeeb30910cc88ee861d4fa8b5a7c1e4
Alex Davis <8m3xlt@gmail.com>
2025-02-20 11:52:01 +0000

    Formatting

--- app/app/mealie.py
+++ app/app/mealie.py
@@ -1,6 +1,7 @@
 import requests
 from .exceptions import *
 
+
 class Mealie:
     def __init__(self, api_key: str, url: str):
         self.api_key = api_key
@@ -14,12 +15,14 @@ class Mealie:
         return response.json()
 
     def _test_api(self) -> None:
-        path =  "/api/app/about"
+        path = "/api/app/about"
         self._get(path)
 
     def _get(self, path: str) -> requests.Response:
         url = self.base_url + path
-        response = requests.get(url, headers = {"Authorization": f"Bearer {self.api_key}"})
+        response = requests.get(url, headers={
+            "Authorization": f"Bearer {self.api_key}"})
         if response.status_code != 200:
-            raise MealieApiError(f"Failed to connect to Mealie with code {response.status_code}.\n{response.text}")
+            raise MealieApiError(
+                f"Failed to connect to Mealie with code {response.status_code}\n{response.text}")
         return response
